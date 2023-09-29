023feb1643acc1c62828b6acf1d8a569e8e8f466
Dumaqkumaq <105821258+Dumaqkumaq@users.noreply.github.com>
2023-09-29 17:07:33 +0300

    Update l1v2.cpp
    Убрал лишние типы данных и инициализировал, а не объявил

--- Лабораторные/l1v2.cpp
+++ Лабораторные/l1v2.cpp
@@ -6,17 +6,16 @@
 */
 int main(){
     //инициализация типов данных и их размеры
-    char ch; // -128 - 126
-    unsigned char un_ch; // 0 - 255
-    short sh; // -32768 - 32767
-    unsigned short un_sh; // 0 - 65536
-    long lng; // -2.147.483.648 - 2.147.483.648
-    unsigned long un_lng; // 0 - 4.294.967.295
-    int in; // -2.147.483.648 - 2.147.483.648
-    unsigned int un_in; // 0 - 4.294.967.295
-    float flo; // -3,4*10**(-38) - 3,4*10**(-38)
-    double doubl; // -3,4*10**(-4932) - 3,4*10**(4932)
-    bool bo; // true или false
+    
+    short sh = 123; // -32768 - 32767
+    unsigned short un_sh = 123; // 0 - 65536
+    long lng = 123; // -2.147.483.648 - 2.147.483.648
+    unsigned long un_lng = 123; // 0 - 4.294.967.295
+    long long lng_lng = 123;
+    int in = 53; // -2.147.483.648 - 2.147.483.648
+    unsigned int un_in = 543; // 0 - 4.294.967.295
+    float flo = 5748; // -3,4*10**(-38) - 3,4*10**(-38)
+    double doubl = 134; // -3,4*10**(-4932) - 3,4*10**(4932)
 
     //используем float из-за возможного ввода дробных чисел
     float square, height;
